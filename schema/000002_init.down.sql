502966092ace3195dc361c7bec41e92daf299c7f
Ko8a <62849086+Ko8a@users.noreply.github.com>
2024-05-28 16:42:21 +0500

    This version of project is now working and has working authorization, login, creating lessons, users, roles, groups and marks

---
+++ schema/000002_init.down.sql
@@ -0,0 +1,17 @@
+DROP TABLE student_performance;
+
+DROP TABLE schedule;
+
+DROP TABLE day_schedule;
+
+DROP TABLE lesson;
+
+DROP TABLE weeks;
+
+DROP TABLE lesson_type;
+
+DROP TABLE users;
+
+DROP TABLE groups;
+
+DROP TABLE role;
